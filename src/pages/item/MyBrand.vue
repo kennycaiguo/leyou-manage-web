65dda26d1395ad4f582add0b33d96b9a724b8ffb
邵益炯 <shaoyijiong@worken.cn>
2018-11-30 17:25:13 +0800

    feat(brand):添加品牌管理

--- src/pages/item/MyBrand.vue
+++ src/pages/item/MyBrand.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card>
     <v-card-title>
-      <v-btn color="primary">
+      <v-btn color="primary" @click="show = true">
         新增品牌
       </v-btn>
       <v-spacer></v-spacer> <!--将新增与搜索分开-->
@@ -10,7 +10,7 @@
         append-icon="search"
         single-line
         class="flex sm3"
-        v-model="search"
+        v-model.lazy="search"
       ></v-text-field>
     </v-card-title>
     <v-divider></v-divider>
@@ -27,29 +27,41 @@
         <td class="text-xs-center">{{ props.item.name }}</td>
         <td class="text-xs-center"><img :src="props.item.image" alt=""/></td>
         <td class="text-xs-center">{{ props.item.letter }}</td>
-        <td class="justify-center layout pt-3">
-          <v-icon color="info"
-                  small
-                  class="mr-2"
-                  @click="editItem(props.item)"
-          >
-            edit
-          </v-icon>
-          <v-icon color="error"
-                  small
-                  @click="deleteItem(props.item)"
-          >
-            delete
-          </v-icon>
+        <td class="justify-center layout px-0">
+          <v-btn icon @click="editBrand(props.item)">
+            <v-icon small>edit</v-icon>
+          </v-btn>
+          <v-btn icon @click="deleteBrand(props.item)">
+            <v-icon small>delete</v-icon>
+          </v-btn>
         </td>
       </template>
     </v-data-table>
+    <v-dialog v-model="show" max-width="500" scrollable persistent>
+      <v-card >
+        <v-card-title class="blue lighten-1 elevation-2">
+          <span class="white--text title">
+            新增品牌
+          </span>
+          <v-spacer/>
+          <v-btn icon class="white--text" @click="show = false">
+            <v-icon>close</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-card-text class="primary--text px-5">
+          <my-brand-form/>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </v-card>
 </template>
 
 <script>
+  import MyBrandForm from "./MyBrandForm";
+
   export default {
     name: "MyBrand",
+    components: {MyBrandForm},
     data() {
       return {
         /*表头*/
@@ -74,7 +86,7 @@
           },
           {
             text: "首字母",
-            value: "name",
+            value: "letter",
             align: 'center',
             sortable: true
           },
@@ -87,44 +99,47 @@
         pagination: {},
         totalBrands: 0,
         loading: false,
-        search:""
+        search: "",
+        show: ""
       }
     },
     watch: {
-      pagination:{
-        deep: true,
-        handle(){
-          this.getDataFormServer();
+      pagination: { // 监视pagination属性的变化
+        deep: true, // deep为true，会监视pagination的属性及属性中的对象属性变化
+        handler() {
+          // 变化后的回调函数，这里我们再次调用getDataFromServer即可
+          this.getDataFromServer();
         }
       },
-      search(){
-          this.getDataFormServer();
-      }
-    },
-    methods: {
-      getDataFormServer() {
-        //开启进度条
-        this.loading = true;
-
-      /* - 请求方式：查询，肯定是Get
-
-        - 请求路径：分页查询，/brand/page
-        - 请求参数：根据我们刚才编写的页面，有分页功能，有排序功能，有搜索过滤功能，因此至少要有5个参数：
-        - page：当前页，int
-        - rows：每页大小，int
-        - sortBy：排序字段，String
-        - desc：是否为降序，boolean
-        - key：搜索关键词，String
-
-        - 响应结果：分页结果一般至少需要两个数据
-        - total：总条数
-        - items：当前页数据
-        - totalPage：有些还需要总页数*/
-        this.loading = false;
+      search: { // 监视搜索字段
+        handler() {
+          this.getDataFromServer();
+        }
       }
     },
     created() {
-      this.getDataFormServer()
+      this.getDataFromServer()
+    },
+    methods: {
+      getDataFromServer() {
+        //开启进度条
+        this.loading = true;
+        //发起ajax请求
+        this.$http.get("/item/brand/page", {
+          params: {
+            desc: this.pagination.descending,
+            rows: this.pagination.rowsPerPage,
+            page: this.pagination.page,
+            sortBy: this.pagination.sortBy,
+            key: this.search
+          }
+        }).then(resp => {
+          this.brands = resp.data.items;
+          this.totalBrands = resp.data.total;
+        }).catch();
+
+        this.loading = false;
+      }
     }
   }
 </script>
